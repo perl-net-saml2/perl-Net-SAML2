3184145f5516a7e7e7caf850dc53752ccfd95671
Mike Wisener <xmikew@cpan.org>
2015-01-08 20:55:37 +0000

    expose xpath object on the assertion

--- lib/Net/SAML2/Protocol/Assertion.pm
+++ lib/Net/SAML2/Protocol/Assertion.pm
@@ -26,6 +26,7 @@ has 'nameid'     => (isa => Str, is => 'ro', required => 1);
 has 'not_before' => (isa => DateTime, is => 'ro', required => 1);
 has 'not_after'  => (isa => DateTime, is => 'ro', required => 1);
 has 'audience'   => (isa => NonEmptySimpleStr, is => 'ro', required => 1);
+has 'xpath'      => (isa => 'XML::Xpath', is => 'ro', required => 1);
 
 =head1 METHODS
 
@@ -66,6 +67,7 @@ sub new_from_xml {
         audience       => $xpath->findvalue('//saml:Conditions/saml:AudienceRestriction/saml:Audience')->value,
         not_before     => $not_before,
         not_after      => $not_after,
+				xpath          => $xpath,
     );
         
     return $self;
