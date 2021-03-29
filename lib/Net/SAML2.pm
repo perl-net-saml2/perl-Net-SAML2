844d0be3201ae1ab1c7ffcbd30679e70ce72459f
Timothy Legge <timlegge@gmail.com>
2021-03-28 23:36:57 -0300

    Update Changes and Increment version

--- lib/Net/SAML2.pm
+++ lib/Net/SAML2.pm
@@ -4,7 +4,7 @@ use warnings;
 
 require 5.008_001;
 
-our $VERSION = '0.32';
+our $VERSION = '0.33';
 $VERSION = eval {$VERSION};
 
 =head1 NAME
--- lib/Net/SAML2/XML/Util.pm
+++ lib/Net/SAML2/XML/Util.pm
@@ -9,7 +9,7 @@ use XML::Tidy;
 use vars qw($VERSION @EXPORT_OK %EXPORT_TAGS $DEBUG);
 
 $DEBUG = 0;
-$VERSION = '0.32';
+$VERSION = '0.33';
 
 # We are exporting functions
 use base qw/Exporter/;
