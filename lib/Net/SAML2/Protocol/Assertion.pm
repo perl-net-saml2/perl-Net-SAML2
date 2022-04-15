92a1eb519f4025a25215e2f2ed1f1bae0a6a8591
Timothy Legge <timlegge@gmail.com>
2022-04-15 16:06:18 -0300

    Support Unsigned encrypted Assertions

--- lib/Net/SAML2/Protocol/Assertion.pm
+++ lib/Net/SAML2/Protocol/Assertion.pm
@@ -103,12 +103,15 @@ sub new_from_xml {
         $xpath      = XML::LibXML::XPathContext->new($dom);
         $xpath->registerNs('saml',  'urn:oasis:names:tc:SAML:2.0:assertion');
         $xpath->registerNs('samlp', 'urn:oasis:names:tc:SAML:2.0:protocol');
+        $xpath->registerNs('dsig', 'http://www.w3.org/2000/09/xmldsig#');
         $xpath->registerNs('xenc', 'http://www.w3.org/2001/04/xmlenc#');
 
         my $xml_opts->{ no_xml_declaration } = 1;
 
         my $assert = $xpath->findnodes('//saml:Assertion')->[0];
-        if (defined $assert) {
+        my @signedinfo = $xpath->findnodes('dsig:Signature', $assert);
+
+        if (defined $assert && (scalar @signedinfo ne 0)) {
             my $x   = Net::SAML2::XML::Sig->new($xml_opts);
             my $ret = $x->verify($assert->serialize);
             die "Decrypted Assertion signature check failed" unless $ret;
